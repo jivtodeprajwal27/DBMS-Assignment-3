c9248f43d4e3c135f003b233e5b3416ff9b0bf9b
Ritesh Patidar <riteshpatidar2499@gmail.com>
2023-03-16 21:20:17 +0530

    added floor

--- app.py
+++ app.py
@@ -1,47 +1,10 @@
-# from flask import Flask, render_template, request, redirect, session, url_for, flash
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_mysqldb import MySQL
-# import MySQLdb.cursors
-# import re
-# #from flaskext.mysql import MySQL
 
 
 # app = Flask(__name__)
 # #app = Flask(__name__, template_folder='template')
 
 # # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/maintenance_portal'
-# #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-# app.secret_key = 'your secret key'
- 
- 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'password'
-# app.config['MYSQL_DB'] = 'maintenance_portal'
-
-# mysql = MySQL(app)
-
-# # def login():
-# #     msg = ''
-# #     if request.method == 'POST' and 'email_id' in request.form and 'password' in request.form:
-# #         email_id = request.form['emial_id']
-# #         password = request.form['password']
-# #         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-# #         cursor.execute('SELECT * FROM user WHERE email_id = % s AND password = % s', (email_id, password, ))
-# #         account = cursor.fetchone()
-# #         if account:
-# #             session['loggedin'] = True
-# #             session['id'] = account['id']
-# #             session['username'] = account['username']
-# #             msg = 'Logged in successfully !'
-# #             return render_template('home.html', msg = msg)
-# #         else:
-# #             msg = 'Incorrect username / password !'
-# #     return render_template('index123.html', msg = msg)
- 
-
+# # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 # # @app.route('/logout')
@@ -51,43 +14,6 @@
 # #    session.pop('username', None)
 # #    return redirect(url_for('login'))
 
-# @app.route('/register', methods =['GET', 'POST'])
-# def register():
-#     msg = ''
-#     if request.method == 'POST' and 'User_id' in request.form and 'Name' in request.form and 'Email_id' in request.form and 'Contact_no' in request.form and 'password' in request.form and 'confirm_password' in request.form:
-#         username = request.form['User_id']
-#         email_id = request.form['Email_id']
-#         name = request.form['Name']
-#         contact_no = request.form['Contact_no']
-
-#         password = request.form['password']
-        
-#         confirm_password = request.form['confirm_password']
-#         cursor = mysql.connection.cursor()
-#         cursor.execute('SELECT * FROM user WHERE email_id = % s', (email_id, ))
-#         account = cursor.fetchone()
-#         if account: 
-#             msg = 'Account already exists !'
-#         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email_id):
-#             msg = 'Invalid email address !'
-#         elif not re.match(r'[A-Za-z0-9]+', username):
-#             msg = 'Username must contain only characters and numbers !'
-#         elif password != confirm_password:
-#             msg = 'Password does not match !'
-#         else:
-#             cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s, % s)', (username, email_id,name,contact_no,password, ))
-#             mysql.connection.commit()
-#             msg = 'You have successfully registered !'
-#     elif request.method == 'POST':
-#         msg = 'Please fill out the form !'
-#     return render_template('index123.html', msg = msg)
-
-
- 
-    
-# if __name__ == "__main__":
-#     # app.run(host ="localhost", port = int("5000"))
-#     app.run(debug=True)
 
 
 
