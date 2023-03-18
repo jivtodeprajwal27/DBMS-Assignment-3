057677384e61b867abd8e9a6a0de9453a9a8b473
SaiKrishna <avulasaikrishna@iitgn.ac.in>
2023-03-19 01:07:48 +0530

    google sign in

--- app.py
+++ app.py
@@ -491,3 +491,66 @@ if __name__ == "__main__":
 
 
 
+# # The user details get print in the console.
+# # so you can do whatever you want to do instead
+# # of printing it
+
+# from flask import Flask, render_template, url_for, redirect
+# from authlib.integrations.flask_client import OAuth
+# import os
+
+# app = Flask(__name__)
+# app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!/xd5\xa2\xa0\x9fR"\xa1\xa8'
+
+# '''
+# 	Set SERVER_NAME to localhost as twitter callback
+# 	url does not accept 127.0.0.1
+# 	Tip : set callback origin(site) for facebook and twitter
+# 	as http://domain.com (or use your domain name) as this provider
+# 	don't accept 127.0.0.1 / localhost
+# '''
+
+# app.config['SERVER_NAME'] = 'localhost:5000'
+# oauth = OAuth(app)
+
+# @app.route('/', methods =['GET', 'POST'])
+# def index():
+# 	return render_template('index123.html')
+
+# @app.route('/google/',  methods =['GET', 'POST'])
+# def google():
+
+# 	# Google Oauth Config
+# 	# Get client_id and client_secret from environment variables
+# 	# For developement purpose you can directly put it
+# 	# here inside double quotes
+# 	GOOGLE_CLIENT_ID = "14105763444-75bvq8dpckghed5kougj1q83538hdn3v.apps.googleusercontent.com"
+# 	GOOGLE_CLIENT_SECRET = "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"
+	
+# 	CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+# 	oauth.register(
+# 		name='google',
+# 		client_id=GOOGLE_CLIENT_ID,
+# 		client_secret=GOOGLE_CLIENT_SECRET,
+# 		server_metadata_url=CONF_URL,
+# 		userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
+# 		client_kwargs={
+# 			'scope': 'openid email profile'
+# 		}
+# 	)
+	
+# 	# Redirect to google_auth function
+# 	redirect_uri = url_for('google_auth', _external=True)
+# 	return oauth.google.authorize_redirect(redirect_uri)
+
+# @app.route('/google/auth/')
+# def google_auth():
+# 	token = oauth.google.authorize_access_token()
+# 	#user = oauth.google.parse_id_token(token)
+# 	#print(" Google User ", user)
+# 	user = oauth.google.userinfo()
+# 	print(user)
+# 	return redirect('/')
+
+# if __name__ == "__main__":
+# 	app.run(debug=True)
